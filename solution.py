b26b8d14fa8930c42c91e2fc6c39d81194db68ad
promax <62617008+promaxV@users.noreply.github.com>
2023-04-13 23:43:14 +0300

    gogo boxcox

--- solution.py
+++ solution.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ttest_ind
+from scipy.stats import ttest_ind, boxcox
 
 chat_id = 664256606  # Ваш chat ID, не меняйте название переменной
 alpha = 0.04
 
 def solution(x, y) -> bool:
-    statistic, p_value = ttest_ind(x, y, alternative="greater")
+    new_x, lm = boxcox(x)
+    statistic, p_value = ttest_ind(new_x, boxcox(y, lmbda=lm), alternative="greater")
     return p_value < alpha
