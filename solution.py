50bf3ba49073bd2d6efafdea5380a4661d51b7ef
promax <62617008+promaxV@users.noreply.github.com>
2023-04-13 21:53:39 +0300

    trying ttest

--- solution.py
+++ solution.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import ttest_ind
 
+chat_id = 664256606  # Ваш chat ID, не меняйте название переменной
+alpha = 0.04
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(x, y) -> bool:
+    statistic, p_value = ttest_ind(x, y, alternative="greater")
+    return p_value < alpha
